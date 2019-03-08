52c5913b008d2835c67a12818c93b9fcaf0f20d0
Long Wang <wangl.cc@outlook.com>
2019-03-08 15:01:33 +0800

    Update install.sh, add the clone for vundle.

--- init.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-mv ~/.vimrc ~/.vimrc_backup
-ln -s $(dirname $(readlink -f $0))/.vimrc ~/.vimrc
--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ mv ~/.vimrc ~/.vimrc_backup
 mv ~/.vim ~/.vim_backup
 ln -s $(dirname $(readlink -f $0))/vimrc ~/.vimrc
 ln -s $(dirname $(readlink -f $0))/vim ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
