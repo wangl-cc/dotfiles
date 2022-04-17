b36e1705a99c06c2a8352d442deda0c8b1e464a7
Long Wang <wangl.cc@outlook.com>
2022-04-17 23:55:28 +0800

    julia: switch project when install missing Pkgs

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -3,8 +3,10 @@ try
 catch e
     if e isa ArgumentError
         import Pkg
-        Pkg.add("Revise")
-        Pkg.add("OhMyREPL")
+        current_project = Pkg.project().path
+        Pkg.activate()
+        Pkg.add(["Revise", "OhMyREPL"])
+        Pkg.activate(current_project)
         using Revise, OhMyREPL
     else
         rethrow()
