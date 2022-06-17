862785def151ccde99915cc2cbd5810aed363e59
Long Wang <wangl.cc@outlook.com>
2022-06-17 18:26:30 +0800

    feat(julia): a simple function to genarate passwd

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -18,3 +18,15 @@ ENV["JULIA_COC_NVIM_DISABLE"] = "false"
 dump1(arg) = dump(arg; maxdepth=1)
 dumpi(arg, i::Integer) = dump(arg; maxdepth=i)
 dumpi(i::Integer) = arg -> dump(arg; maxdepth=i)
+
+using Random
+
+const UpperCases = 65:90
+const LowerCases = 97:122
+const Digits = 48:57
+const Hyphen = 45
+const Underscore = 95
+const DefaultAlloweds = UInt8[UpperCases; LowerCases; Digits; Hyphen; Underscore]
+
+gen_passwd(length::Integer=12, symbols::Vector{<:Integer}=DefaultAlloweds) =
+    String(rand(Random.RandomDevice(), symbols, length))
