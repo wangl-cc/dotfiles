cf55774deb1a78bc2278a8b10f65bf5b23f1298f
Long Wang <wangl.cc@outlook.com>
2022-08-22 03:07:27 +0000

    rename variables

--- install.sh
+++ install.sh
@@ -1,23 +1,22 @@
 #!/bin/zsh
 
-# prepare paths
-__RC_PREFIX="$HOME/.local"
-__RC_BINPATH="$__RC_PREFIX/bin"
-if ! [ -d "$__RC_BINPATH" ]; then
-    mkdir -p "$__RC_BINPATH"
+PREFIX=${PREFIX-"$HOME/.local"}
+BINPATH=${BINPATH-"$PREFIX/bin"}
+if ! [ -d "$BINPATH" ]; then
+    mkdir -p "$BINPATH"
 fi
-__RC_MANPATH="$__RC_PREFIX/share/man"
-__RC_COMPPATH="$__RC_PREFIX/share/zsh/site-functions"
-if ! [ -d $__RC_COMPPATH ]; then
-    mkdir -p $__RC_COMPPATH
+MANPATH=${MANPATH-"$PREFIX/share/man"}
+COMPPATH=${COMPPATH-"$PREFIX/share/zsh/site-functions"}
+if ! [ -d $COMPPATH ]; then
+    mkdir -p $COMPPATH
 fi
-__RC_CACHEPATH="$HOME/.cache/zshrc"
-if ! [ -d $__RC_CACHEPATH ]; then
-    mkdir -p $__RC_CACHEPATH
+CACHEPATH=${CACHEPATH-"$HOME/.cache/zshrc"}
+if ! [ -d $CACHEPATH ]; then
+    mkdir -p $CACHEPATH
 fi
 
 # install functions
-__rc_install() {
+install_base() {
     url="$1"
     dest="$2"
     mod="$3"
@@ -26,52 +25,57 @@ __rc_install() {
     print -P "URL: %F{33}$url%f"
     print -P "Destination: %F{33}$dest/$name%f"
     print -P "Mode: %F{33}$mod%f"
-    curl -sSLo "$__RC_CACHEPATH/$name" "$url" && \
-        install -m $mod "$__RC_CACHEPATH/$name" "$dest" && \
+    curl -sSLo "$CACHEPATH/$name" "$url" && \
+        install -m $mod "$CACHEPATH/$name" "$dest" && \
         print -P "Installation successful.%b" || \
         print -P "Installation failed.%b"
 }
-__rc_install_bin() { __rc_install $1 $__RC_BINPATH 755; }
-__rc_install_comp() { __rc_install $1 $__RC_COMPPATH 644; }
-__rc_install_man() {
-    __rc_manpath="$__RC_MANPATH/man${1##*.}"
-    if ! [ -d $__rc_manpath ]; then
-        mkdir -p $__rc_manpath
+install_bin() { install_base $1 $BINPATH 755; }
+install_comp() { install_base $1 $COMPPATH 644; }
+install_man() {
+    MANPATH="$MANPATH/man${1##*.}"
+    if ! [ -d $MANPATH ]; then
+        mkdir -p $MANPATH
     fi
-    __rc_install $1 $__rc_manpath 644
+    install_base $1 $MANPATH 644
 }
 
 # install yadm
-__RC_YADM_TAG="3.2.1"
-__RC_YADM_RAW=${YADM_RAW-'https://raw.githubusercontent.com/TheLocehiliosan/yadm'}
-__rc_install_yadm() {
-    __rc_install_bin "$__RC_YADM_RAW/$__RC_YADM_TAG/yadm"
-    __rc_install_comp "$__RC_YADM_RAW/$__RC_YADM_TAG/completion/zsh/_yadm"
-    __rc_install_man "$__RC_YADM_RAW/$__RC_YADM_TAG/yadm.1"
+YADM_TAG=${YADM_TAG-"3.2.1"}
+YADM_RAW=${YADM_RAW-'https://raw.githubusercontent.com/TheLocehiliosan/yadm'}
+install_yadm() {
+    install_bin "$YADM_RAW/$YADM_TAG/yadm"
+    install_comp "$YADM_RAW/$YADM_TAG/completion/zsh/_yadm"
+    install_man "$YADM_RAW/$YADM_TAG/yadm.1"
 }
 if test ! $(command -v yadm); then
-    __rc_install_yadm
+    install_yadm
 fi
 # install esh
-__RC_ESH_TAG="v0.3.2"
-__RC_ESH_RAW=${ESH_RAW-'https://raw.githubusercontent.com/jirutka/esh'}
-__rc_install_esh() {
-    __rc_install_bin "$__RC_ESH_RAW/$__RC_ESH_TAG/esh"
+ESH_TAG=${YADM_TAG-"v0.3.2"}
+ESH_RAW=${ESH_RAW-'https://raw.githubusercontent.com/jirutka/esh'}
+install_esh() {
+    install_bin "$ESH_RAW/$ESH_TAG/esh"
 }
 if test ! $(command -v esh); then
-    __rc_install_esh
+    install_esh
 fi
 
 # set PATH
 typeset -U PATH path
-path=("$__RC_BINPATH" "$path[@]")
+path=("$BINPATH" "$path[@]")
 export PATH
 
 # clone repo
 if [ -f "$HOME/.zshrc" ]; then
+    echo "Found existing .zshrc file, backing up to .zshrc.bak"
     mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
 fi
-__REPO_URL=${REPO-git@github.com:wangl-cc/dotfiles.git}
-yadm clone $__REPO_URL --bootstrap
+GITHUB=${GITHUB-"git@github.com:"}
+OWNER=${OWNER-"wangl-cc"}
+REPO=${REPO-"dotfiles"}
+REPO_URL=${REPO_URL-"$GITHUB$OWNER/$REPO.git"}
+yadm clone $REPO_URL --no-bootstrap
 # move repo
 mv $HOME/.local/share/yadm/repo.git $HOME/.git
+yadm --yadm-repo $HOME/.git bootstrap
