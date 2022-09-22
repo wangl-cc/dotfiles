b3d0226f4dadf384b1e0b6b203639397f55c8983
Long Wang <wangl.cc@outlook.com>
2022-09-23 04:03:04 +0800

    feat(vim): add iron to send code to REPL

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -122,6 +122,51 @@ packer.startup(function(use)
       }
     end,
   }
+  --- Interactive REPL
+  use {
+    'hkupty/iron.nvim',
+    tag = '*',
+    config = function()
+      local iron = require('iron.core')
+      iron.setup {
+        config = {
+          repl_open_cmd = function(bufnr)
+            vim.api.nvim_buf_set_option(bufnr, 'filetype', 'iron')
+            return require('iron.view').split.vertical.botright(80, {
+              number = false,
+              relativenumber = false,
+            })(bufnr)
+          end,
+          repl_definition = {
+            julia = {
+              command = { 'julia', '--project' }
+            }
+          },
+        },
+        keymaps = {
+          send_motion = '<leader>sc',
+          visual_send = '<leader>sc',
+          send_file = '<leader>sf',
+          send_line = '<leader>sl',
+          send_mark = '<leader>sm',
+          mark_motion = '<leader>mc',
+          mark_visual = '<leader>mc',
+          remove_mark = '<leader>md',
+          cr = '<leader>s<cr>',
+          interrupt = '<leader>s<space>',
+          exit = '<leader>sq',
+          clear = '<leader>cl',
+        },
+      }
+      vim.keymap.set('n', '<leader>tr', function()
+        if vim.bo.filetype == 'iron' then
+          vim.api.nvim_win_hide(0)
+        else
+          iron.repl_for(vim.bo.ft)
+        end
+      end, { silent = true, noremap = true })
+    end
+  }
   --- Input and select
   use {
     'stevearc/dressing.nvim',
