71fd8a0f2a6917120a4ffbb758a2cfa7262b2c0e
Long Wang <wangl.cc@outlook.com>
2022-10-21 21:49:01 +0800

    feat(vim): disable blend of terminal popup

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -134,7 +134,6 @@ local function startup(use)
         direction = 'float',
         float_opts = {
           border = 'rounded',
-          winblend = 10,
           width = math.floor(vim.o.columns * 0.8),
           height = math.floor(vim.o.lines * 0.8),
         },
