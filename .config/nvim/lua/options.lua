525f01afbd9ba51f302692ff1ba51440eef3db68
Long Wang <wangl.cc@outlook.com>
2022-10-22 21:57:32 +0800

    refractor(vim): better option setting

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,5 +1,6 @@
 -- shortcuts for the most common functions
 local g = vim.g
+local o = vim.o
 local opt = vim.opt
 local cmd = vim.cmd
 local map = vim.keymap.set
@@ -28,72 +29,76 @@ cmd 'syntax on'
 cmd 'highlight Comment cterm=italic gui=italic'
 
 -- use true colors
-opt.termguicolors = true
+o.termguicolors = true
 
 -- show line number and relative line number
-opt.number = true
-opt.relativenumber = true
+o.number = true
+o.relativenumber = true
 
 -- asking when quit
-opt.confirm = true
+o.confirm = true
 
 -- when a bracket is inserted, briefly jump to the matching one
-opt.showmatch = true
+o.showmatch = true
 
 -- file encoding
-opt.fileencoding = 'utf-8'
+o.fileencoding = 'utf-8'
 
 -- search
-opt.hlsearch = true
-opt.incsearch = true
-opt.smartcase = true
+o.hlsearch = true
+o.incsearch = true
+o.smartcase = true
 map('n', '<leader>/', [[:nohlsearch<CR>:match<CR>]], silent_noremap 'nohlsearch and clear match')
 
 -- split
-opt.splitbelow = true
-opt.splitright = true
+o.splitbelow = true
+o.splitright = true
 
 -- Indent
-opt.expandtab = true
-opt.smarttab = true
-opt.autoindent = true
+o.expandtab = true
+o.smarttab = true
+o.autoindent = true
 
 -- spell
-opt.spelloptions = 'camel,noplainbuffer'
+o.spelloptions = 'camel,noplainbuffer'
 
 -- folding
-opt.foldexpr = 'nvim_treesitter#foldexpr()'
-opt.foldnestmax = 3
+o.foldexpr = 'nvim_treesitter#foldexpr()'
+o.foldnestmax = 3
 
 -- highlight current line
-opt.cursorline = true
+o.cursorline = true
 
 -- keep 3 lines above and below the cursor
-opt.scrolloff = 3
+o.scrolloff = 3
 
 -- disable error bell
-opt.errorbells = false
+o.errorbells = false
 
 -- hide cmdline
-opt.cmdheight = 0
+o.cmdheight = 0
 
 -- check modeline
-opt.modelines = 1
+o.modelines = 1
 
 -- always show tab line
-opt.showtabline = 2
+o.showtabline = 2
 
 -- always show a global status line, require nvim-0.7
-opt.laststatus = 3
+o.laststatus = 3
 
 -- display sign in number column
-opt.signcolumn = 'number'
+o.signcolumn = 'number'
 
 -- gui cursor, only works when 'termguicolors' is on
 -- The 'guicursor' don't use the highlight group by default,
 -- this is a modified version of the default 'guicursor' with highlight group
 -- more see: h 'guicursor'
-opt.guicursor = 'n-v-c-sm:block-Cursor/lCursor,i-ci-ve:ver25-Cursor/lCursor,r-cr-o:hor20-Cursor/lCursor'
+opt.guicursor = {
+  'n-v-c-sm:block-Cursor/lCursor',
+  'i-ci-ve:ver25-Cursor/lCursor',
+  'r-cr-o:hor20-Cursor/lCursor',
+}
 
 -- tex flavor
 g.tex_flavor = 'latex'
