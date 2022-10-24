bdcf6950bc7a9ecf8147cd84498b8fc3bd17ad2f
Long Wang <wangl.cc@outlook.com>
2022-10-24 19:00:26 +0800

    fix(vim): no need to escape `\` in lua string

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -6,6 +6,6 @@ end
 
 --- mapppings
 map('n', '<leader>/', [[:nohlsearch<CR>:match<CR>]], silent 'nohlsearch and clear match')
-map('n', '<leader>db', [[:%s/[ \\t]\\+$//<CR>]], silent 'Remove trailing blanks')
+map('n', '<leader>db', [[:%s/[ \t]\+$//<CR>]], silent 'Remove trailing blanks')
 map('n', '<leader>cw', [[: %s/\V\<<C-r><C-w>\>/<C-r><C-w>]], { desc = 'Change all matchs of cword' })
 map('n', '<leader>cW', [[: %s/\V<C-r><C-a>/<C-r><C-a>]], { desc = 'Change all matchs of cWORD' })
