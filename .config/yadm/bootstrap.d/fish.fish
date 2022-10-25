320446a0b2a665d45bb44f33b9b2fb455ff05c6a
Long Wang <wangl.cc@outlook.com>
2022-10-25 17:18:06 +0800

    feat(yadm): better bootstrap with verbose

---
+++ .config/yadm/bootstrap.d/fish.fish
@@ -0,0 +1,18 @@
+echo -e "\n\033[1;35mSetting up fish shell...\033[0m"
+
+function set_verbose
+  echo "  set" $argv
+  set $argv
+end
+
+if type -q brew
+  set_verbose -Ux HOMEBREW_PREFIX (brew --prefix)
+  set_verbose -Ux HOMEBREW_CELLAR (brew --cellar)
+  set_verbose -Ux HOMEBREW_REPOSITORY (brew --repo)
+  set -l _homebrew_bin $HOMEBREW_PREFIX/bin
+  if not contains $_homebrew_bin $fish_user_paths
+    set_verbose -U fish_user_paths $_homebrew_bin $fish_user_paths
+  end
+end
+
+# vim:ts=2:sw=2:et
---
+++ .config/yadm/bootstrap.d/macOS.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo -e "\n\033[1;35mSetting up macOS defaults...\033[0m"
+
+defaults_verbose() {
+  echo "  defaults $*"
+  defaults "$@"
+}
+
+# iterm2
+## set iterm2 config folder
+defaults_verbose write com.googlecode.iterm2 PrefsCustomFolder -string "$HOME/.iterm"
+## enable custom folder
+defaults_verbose write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+
+# disbale some annoying input automation
+defaults_verbose write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults_verbose write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults_verbose write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults_verbose write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# trackpad
+for domain in com.apple.driver.AppleBluetoothMultitouch.trackpad \
+  com.apple.AppleMultitouchTrackpad; do
+  ## enable tap to click
+  defaults_verbose write "$domain" Clicking -bool true
+  ## twi finger tap to emulate right click
+  defaults_verbose write "$domain" TrackpadRightClick -bool true
+  defaults_verbose write "$domain" TrackpadTwoFingerDoubleTapGesture -bool true
+  ## two finger edge swipe
+  defaults_verbose write "$domain" TrackpadTwoFingerFromRightEdgeSwipeGesture -int 3
+  ## three finger drag
+  defaults_verbose write "$domain" TrackpadThreeFingerDrag -int 1
+  defaults_verbose write "$domain" TrackpadThreeFingerHorizSwipeGesture -int 0
+  defaults_verbose write "$domain" TrackpadThreeFingerVertSwipeGesture -int 0
+  ## three finger tap for search
+  defaults_verbose write "$domain" TrackpadThreeFingerTapGesture -int 2
+  ## four finger swipe gestures
+  defaults_verbose write "$domain" TrackpadFourFingerHorizSwipeGesture -int 2
+  defaults_verbose write "$domain" TrackpadFourFingerVertSwipeGesture -int 2
+  ## four finger pinch gestures
+  defaults_verbose write "$domain" TrackpadFourFingerPinchGesture -int 2
+done
+
+# dock
+## disbale magnification
+defaults_verbose write com.apple.dock magnification -bool false
+
+# vscode
+## enable keys repeat when hold
+defaults_verbose write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+# don't save to iCloud by default
+defaults_verbose write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# vim:ts=2:sw=2:et
