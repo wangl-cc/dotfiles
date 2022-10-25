0fa1d8a6ff1baa34e255df51905b385e490bfd90
Long Wang <wangl.cc@outlook.com>
2022-10-25 22:02:02 +0800

    feat: use the same shell for remote and local

--- .config/fish/functions/sshr.fish
+++ .config/fish/functions/sshr.fish
@@ -2,9 +2,9 @@ function sshr --description "ssh with remote forward"
   set -l port (random 10000 30000)
   printf "Use port %d for remote forward" $port
   ssh -t -R $port:localhost:22 -o RemoteCommand="
-  export SHELL=/home/%r/.local/bin/zsh \
+  export SHELL=/home/%r/.local/bin/$(basename $SHELL) \
     SSHR_PORT=$sshr_port \
-    LC_HOST=$USER\@localhost LC_OS=$(uname -s) \
+    LC_HOST=$USER\@localhost LC_OS=(uname -s) \
     TERM_PROGRAM=$TERM_PROGRAM;
   exec \$SHELL -l" $argv
 end
