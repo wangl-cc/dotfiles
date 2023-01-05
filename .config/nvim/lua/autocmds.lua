42f1c51c049d3bf65bd2fe2b857ffc6ed1fe4c5a
Long Wang <wangl.cc@outlook.com>
2023-01-05 09:54:40 +0800

    refactor(vim): autocmds

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,55 +1,57 @@
--- set auto delete buffer for some filetypes
-local auto_bd = vim.api.nvim_create_augroup("AutoDeleteBuffer", { clear = true })
+local group = vim.api.nvim_create_augroup("UserCmds", { clear = true })
+
+-- close some filetypes with <q> and auto delete buffer for some filetypes
+local close_with_q = function(buf)
+  vim.keymap.set("n", "q", "<Cmd>close<CR>", { buffer = buf, silent = true })
+end
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "checkhealth" },
-  callback = function()
-    vim.bo.bufhidden = "delete"
+  callback = function(args)
+    vim.bo[args.buf].bufhidden = "delete"
+    close_with_q(args.buf)
   end,
-  group = auto_bd,
+  group = group,
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "lsp.log", "mason.log" },
+  callback = function(args)
+    vim.bo[args.buf].bufhidden = "delete"
+    close_with_q(args.buf)
+  end,
+  group = group,
 })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "gitcommit", "gitrebase" },
-  callback = function()
-    vim.bo.bufhidden = "wipe"
+  callback = function(args)
+    vim.bo[args.buf].bufhidden = "wipe"
   end,
-  group = auto_bd,
+  group = group,
 })
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = { "lsp.log" },
-  callback = function()
-    vim.bo.bufhidden = "delete"
-  end,
-  group = auto_bd,
-})
-
--- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
   pattern = {
     "help",
     "man",
-    "notify",
-    "startuptime",
     "tsplayground",
+    "git", -- gitdiff
   },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  callback = function(args)
+    close_with_q(args.buf)
   end,
-  group = vim.api.nvim_create_augroup("CloseWithQ", { clear = true }),
+  group = group,
 })
 
--- set tabstop and shiftwidth for some filetype
+-- set different tabstop and shiftwidth for some filetype
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "julia", "python" },
   callback = function()
     vim.bo.tabstop = 4
     vim.bo.shiftwidth = 4
   end,
-  group = vim.api.nvim_create_augroup("IndentOptions", { clear = true }),
+  group = group,
 })
 
--- show cursor line only in active window
-local cursorline = vim.api.nvim_create_augroup("CursorLine", { clear = true })
+-- Show cursor line only in active window
 -- There is a win variable `cursorline` to store cursorline status when disable it
 -- this should avoid to enable cursorline for some window
 -- where the cursorline is disable, such as Telescope prompt
@@ -61,7 +63,7 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
       vim.api.nvim_win_del_var(0, "cursorline")
     end
   end,
-  group = cursorline,
+  group = group,
 })
 vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
   callback = function()
@@ -71,11 +73,10 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
       vim.wo.cursorline = false
     end
   end,
-  group = cursorline,
+  group = group,
 })
 
 -- treesitter for esh
-local esh = vim.api.nvim_create_augroup("ESH", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "esh_*" },
   callback = function(args)
@@ -92,5 +93,5 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.treesitter.query.set_query("embedded_template", "injections", scm)
     vim.treesitter.start(buffer, "embedded_template")
   end,
-  group = esh,
+  group = group,
 })
