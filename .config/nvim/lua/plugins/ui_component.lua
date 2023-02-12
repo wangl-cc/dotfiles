wangl-cc/dotfiles

commit 9c7c64404dd10c021d673829af12fee359b9d442

feat(vim): update plugins and update related plugin configs
- Update `lazy.nvim` to `v9`, which deprecates setting a table to `config`
- Change deprecates `config` to `opts`
- Update other plugins

.config/nvim/lua/plugins/ui_component.lua
@@ -2,7 +2,7 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     event = "UIEnter",
-    config = {
+    opts = {
       char = "▏",
       context_char = "▏",
       show_current_context = true,
@@ -13,7 +13,7 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     version = "2",
     cmd = "Neotree",
-    config = {
+    opts = {
       close_if_last_window = true,
       popup_border_style = "rounded",
       sort_case_insensitive = true,
@@ -36,7 +36,7 @@ return {
   {
     "TimUntersberger/neogit",
     cmd = "Neogit",
-    config = {
+    opts = {
       kind = "vsplit",
       -- customize displayed signs
       signs = {
@@ -57,7 +57,7 @@ return {
     },
     config = function()
       require("iron.core").setup {
-        config = {
+        opts = {
           highlight_last = false,
           repl_open_cmd = function(bufnr)
             -- HACK: set the filetype to 'iron' to detect it when needed
@@ -88,7 +88,7 @@ return {
   {
     "akinsho/toggleterm.nvim",
     cmd = "ToggleTerm",
-    config = {
+    opts = {
       size = 12,
       shade_terminals = false,
       start_in_insert = true,

.config/nvim/lua/plugins/editing.lua
@@ -72,7 +72,7 @@ return {
   },
   {
     "zbirenbaum/copilot.lua",
-    config = {
+    opts = {
       filetypes = {
         help = false,
         iron = false,

.config/nvim/lua/plugins/init.lua
@@ -1,6 +1,6 @@
 return {
   -- Lazy can manage itself
-  { "folke/lazy.nvim", version = "7" },
+  { "folke/lazy.nvim", version = "9" },
 
   -- common dependencies
   { "nvim-lua/plenary.nvim" },

.config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
       "williamboman/mason.nvim",
       {
         "williamboman/mason-lspconfig.nvim",
-        config = {
+        opts = {
           automatic_installation = {
             exclude = {
               "julials", -- I use my own config of julials
@@ -25,7 +25,7 @@ return {
   },
   {
     "williamboman/mason.nvim",
-    config = {
+    opts = {
       ui = {
         -- TODO: change size of floating window
         border = "rounded",
@@ -44,7 +44,7 @@ return {
       "williamboman/mason.nvim",
       {
         "jayp0521/mason-null-ls.nvim",
-        config = {
+        opts = {
           ensure_installed = { "stylua", "gitlint" },
         },
       },

.config/nvim/lua/plugins/ui_api.lua
@@ -7,7 +7,7 @@ return {
   {
     "stevearc/dressing.nvim",
     event = "UIEnter",
-    config = {
+    opts = {
       -- vim.ui.input is handled by noice
       input = { enabled = false },
       select = {
@@ -19,7 +19,7 @@ return {
   -- Better vim.notify
   {
     "rcarriga/nvim-notify",
-    config = {
+    opts = {
       stages = "fade",
       timeout = 3000,
       level = vim.log.levels.INFO,

commit 7f064eb7de69cc664c3828a5f30923ef0c9c34af

fix(vim): iron config
The iron config was broken in 9c7c64404dd10c021d673829af12fee359b9d442
because the config key was renamed to opts by mistake.

.config/nvim/lua/plugins/ui_component.lua
@@ -57,7 +57,7 @@ return {
     },
     config = function()
       require("iron.core").setup {
-        opts = {
+        config = {
           highlight_last = false,
           repl_open_cmd = function(bufnr)
             -- HACK: set the filetype to 'iron' to detect it when needed