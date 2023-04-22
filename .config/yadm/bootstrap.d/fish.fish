cf650629e26c5e0e8f6f6b075fcd3fa9043cbf7e
Long Wang <wangl.cc@outlook.com>
2023-04-23 00:07:57 +0800

    feat(yadm): set fish_user_paths with fish_add_path in bootstrap

--- .config/yadm/bootstrap.d/fish.fish
+++ .config/yadm/bootstrap.d/fish.fish
@@ -5,35 +5,13 @@ function set_verbose
   set $argv
 end
 
-function set_user_path
-  set -l paths
-  for path in $argv
-    if not contains $path $fish_user_paths
-      set paths $path $paths
-    end
-  end
-  if test -n "$paths"
-    set_verbose -U fish_user_paths $paths $fish_user_paths
-  end
-end
-
-function set_existent_path
-  for path in $argv
-    if test -d $path
-      set_user_path $path
-    end
-  end
-end
-
 if type -q brew
   set_verbose -Ux HOMEBREW_PREFIX (brew --prefix)
   set_verbose -Ux HOMEBREW_CELLAR (brew --cellar)
   set_verbose -Ux HOMEBREW_REPOSITORY (brew --repo)
-  set_user_path $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
+  fish_add_path -Up $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
 end
 
-set_user_path $HOME/.local/bin
-
-set_existent_path $HOME/.cargo/bin $HOME/.local/share/bob/nvim-bin
+fish_add_path -Up $HOME/.local/bin $HOME/.cargo/bin $HOME/.local/share/bob/nvim-bin
 
 # vim:ts=2:sw=2:et
