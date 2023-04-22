7d34e59b7873458d533b1e6f33163d3967412c5d
Long Wang <wangl.cc@outlook.com>
2023-04-23 00:18:18 +0800

    feat(yadm): setup homebrew mirror

---
+++ .config/yadm/bootstrap.d/brew.sh
@@ -0,0 +1,34 @@
+echo -e "\n\033[1;35mSetting up homebrew...\033[0m"
+
+default_git_remote="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+env_git_remote="${HOMEBREW_BREW_GIT_REMOTE:-"$default_git_remote"}" # useful for reset to default
+homebrew_mirror=$(dirname "$env_git_remote")
+
+brew="$(brew --repo)"
+brew_url=$(git -C "$brew" remote get-url origin)
+brew_mirror="$homebrew_mirror/brew.git"
+if [[ "$brew_url" != "$brew_mirror" ]]; then
+  export HOMEBREW_BREW_GIT_REMOTE="$brew_mirror"
+fi
+
+brew_core="$(brew --repo homebrew/core)"
+brew_core_url=$(git -C "$brew_core" remote get-url origin)
+brew_core_mirror="$homebrew_mirror/homebrew-core.git"
+if [[ "$brew_core_url" != "$brew_core_mirror" ]]; then
+  export HOMEBREW_CORE_GIT_REMOTE="$brew_core_mirror"
+  brew tap --force-auto-update homebrew/core
+fi
+
+for tap in cask{,-fonts,-drivers,-versions} command-not-found services; do
+  repo=$(brew --repo "homebrew/$tap")
+  if [ -d "$repo" ]; then
+    url=$(git -C "$repo" remote get-url origin)
+    mirror="$homebrew_mirror/homebrew-$tap.git"
+    if [[ "$url" != "$mirror" ]]; then
+      brew tap --custom-remote --force-auto-update "homebrew/$tap" \
+        "$homebrew_mirror/homebrew-$tap.git"
+    fi
+  fi
+done
+
+brew update
--- .config/yadm/bootstrap.d/fish.fish
+++ .config/yadm/bootstrap.d/fish.fish
@@ -10,6 +10,8 @@ if type -q brew
   set_verbose -Ux HOMEBREW_CELLAR (brew --cellar)
   set_verbose -Ux HOMEBREW_REPOSITORY (brew --repo)
   fish_add_path -Up $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
+  set_verbose -Ux HOMEBREW_API_DOMAIN "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+  set_verbose -Ux HOMEBREW_BOTTLE_DOMAIN "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
 end
 
 fish_add_path -Up $HOME/.local/bin $HOME/.cargo/bin $HOME/.local/share/bob/nvim-bin
