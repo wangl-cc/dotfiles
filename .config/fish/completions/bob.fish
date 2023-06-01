abae5c7f645bcb7c94f18c2a3ec50e3476684ad0
Long Wang <wangl.cc@outlook.com>
2023-06-01 10:55:13 +0800

    feat(fish): improve bob completions
    - Use native fish completion functions
    - Fixed use and rm completion for new bob version
    - Added sync and rollback completion
    - Allow alaises in help completion

--- .config/fish/completions/bob.fish
+++ .config/fish/completions/bob.fish
@@ -1,44 +1,31 @@
 # completion for bob, a version manager for neovim
 complete -c bob -e
 
-function __fish_bob_needs_command
-  set -l cmd (commandline -opc)
-  set -e cmd[1]
-  if set -q cmd[1]
-    return 1
-  end
-  return 0
-end
-
-function __fish_bob_using_command
-  set -l cmd (commandline -opc)
-  set -e cmd[1]
-  if set -q cmd[1]; and contains -- $cmd[1] $argv
-    return 0
-  end
-  return 1
-end
-
+# Options
+# -h, --help, it's available for bob and all subcommands
 complete -f -c bob -s h -l help -d "Print help information"
-complete -f -c bob -n "__fish_bob_needs_command" -s V -l version -d "Print version information"
+# -V, --version, it's only available for bob
+complete -f -c bob -n "__fish_use_subcommand" -s V -l version -d "Print version information"
 
-#  ls is aliased to list and uninstall is aliased to rm
+# ls is aliased to list and uninstall is aliased to rm
 # These are not included in the completion list to make sure
 # that first characters are unique
-set -l subcmds use install erase rm list help 
-complete -f -c bob -n "__fish_bob_needs_command" -a use -d "Use a specific version of neovim"
-complete -f -c bob -n "__fish_bob_needs_command" -a install -d "Install the specific versions of neovim"
-complete -f -c bob -n "__fish_bob_needs_command" -a rm -d "Uninstall the specific version of neovim"
-complete -f -c bob -n "__fish_bob_needs_command" -a erase -d "Erase any changes made by bob"
-complete -f -c bob -n "__fish_bob_needs_command" -a list -d "List all installed and used versions of neovim"
-complete -f -c bob -n "__fish_bob_needs_command" -a help -d "Print help of bob or a specific command"
+set -l subcmds use install sync uninstall rm rollback erase list ls help
+complete -f -c bob -n "__fish_use_subcommand" -a use -d "Use a specific version of neovim"
+complete -f -c bob -n "__fish_use_subcommand" -a install -d "Install the specific versions of neovim"
+complete -f -c bob -n "__fish_use_subcommand" -a sync -d "Sync the installed versions of neovim with the versions in the config file"
+complete -f -c bob -n "__fish_use_subcommand" -a rm -d "Uninstall the specific version of neovim"
+complete -f -c bob -n "__fish_use_subcommand" -a rollback -d "Rollback to and existing nightly rollback"
+complete -f -c bob -n "__fish_use_subcommand" -a erase -d "Erase any changes made by bob"
+complete -f -c bob -n "__fish_use_subcommand" -a list -d "List all installed and used versions of neovim"
+complete -f -c bob -n "__fish_use_subcommand" -a help -d "Print help of bob or a specific command"
 
 # we can only use and uninstall if there is a version installed and not used
 function __bob_list_installed
-  echo (bob list | grep "Installed" | cut -d  "|" -f 1)
+  bob list | sed '1,3d' | sed '$d' | awk '{print $2}'
 end
 
-complete -f -c bob -n "__fish_bob_using_command help" -a "$subcmds"
-complete -f -c bob -n "__fish_bob_using_command use rm" -a "$(__bob_list_installed)"
-complete -f -c bob -n "__fish_bob_using_command install" -a "nightly stable"
-complete -f -c bob -n "__fish_bob_using_command list erase" # prevent completion
+complete -f -c bob -n "__fish_seen_subcommand_from help" -a "$subcmds"
+complete -f -c bob -n "__fish_seen_subcommand_from use rm" -a "$(__bob_list_installed)"
+complete -f -c bob -n "__fish_seen_subcommand_from install" -a "nightly stable"
+complete -f -c bob -n "__fish_seen_subcommand_from sync rollback list erase" # prevent completion
