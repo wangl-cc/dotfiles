89777421d8094a08e8052eb00e095864a4a9b5da
Long Wang <wangl.cc@outlook.com>
2023-07-12 01:35:31 +0800

    feat(vim)!: remove friendly-snippets
    I don't use most of snippets, and some snippets are annoy.

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -10,7 +10,6 @@ local M = {
     { --- Snippets
       "L3MON4D3/LuaSnip",
       build = "make install_jsregexp",
-      dependencies = "rafamadriz/friendly-snippets",
       config = function() require("luasnip/loaders/from_vscode").lazy_load() end,
     },
   },
