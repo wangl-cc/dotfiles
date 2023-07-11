63e0e109ddd3a61970b825a1581a6a97e3049453
Long Wang <wangl.cc@outlook.com>
2023-07-12 01:23:21 +0800

    feat(vim): add build for luasnip
    Which is required for lsp-snippet-transformations.

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -9,6 +9,7 @@ local M = {
     "saadparwaiz1/cmp_luasnip",
     { --- Snippets
       "L3MON4D3/LuaSnip",
+      build = "make install_jsregexp",
       dependencies = "rafamadriz/friendly-snippets",
       config = function() require("luasnip/loaders/from_vscode").lazy_load() end,
     },
