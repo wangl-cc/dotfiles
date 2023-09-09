c846ec3d4edc981b2e8033893b14388d8dff5670
Loong <wangl.cc@outlook.com>
2023-09-10 00:34:08 +0800

    feat: change some icons

--- .config/nvim/lua/util/icons.lua
+++ .config/nvim/lua/util/icons.lua
@@ -30,40 +30,40 @@ icons.kinds = {
 
 -- stylua: ignore
 icons.diagnostic = {
-  error = " ", -- nf-fa-times_circle
-  warn  = " ", -- nf-fa-exclamation_circle
-  info  = " ", -- nf-fa-info_circle
-  hint  = " ", -- nf-fa-check_circle
+  error = "󰅚 ", -- nf-md-close_circle_outline
+  warn  = "󰗖 ", -- nf-md-alert_circle_outline
+  info  = "󰋽 ", -- nf-md-information_outline
+  hint  = "󰘥 ", -- nf-md-help_circle_outline
 }
 
 -- stylua: ignore
 icons.loglevel = {
-  ERROR = "", -- nf-fa-times_circle
-  WARN  = "", -- nf-fa-exclamation_circle
-  INFO  = "", -- nf-fa-info_circle
-  DEBUG = "", -- nf-fa-bug
-  TRACE = "✎", -- U+270E
+  ERROR = "󰅚 ", -- nf-md-close_circle_outline
+  WARN  = "󰗖 ", -- nf-md-alert_circle_outline
+  INFO  = "󰋽 ", -- nf-md-information_outline
+  DEBUG = " ", -- nf-fa-bug
+  TRACE = " ", -- nf-fac-footprint
 }
 
 -- stylua: ignore
 icons.diff = {
-  added    = " ", -- nf-fa-plus_circle
-  removed  = " ", -- nf-fa-minus_circle
-  modified = " ", -- nf-fa-circle_o
+  added    = "󰐙 ", -- nf-md-plus_circle_outline
+  removed  = "󰍷 ", -- nf-md-minus_circle_outline
+  modified = "󰝶 ", -- nf-md-pencil_circle_outline
 }
 
 -- stylua: ignore
 icons.file_status = {
-  modified = " ", -- nf-fa-circle
-  readonly = " ", -- nf-fa-lock
-  new      = " ", -- nf-fa-plus_circle
+  modified = "󰧟 ", -- nf-md-circle_medium
+  readonly = "󰌾 ", -- nf-md-lock
+  new      = "󰐙 ", -- nf-md-plus_circle_outline
 }
 
 -- stylua: ignore
 icons.package = {
-  package_installed   = " ", -- nf-fa-circle
-  package_pending     = " ", -- nf-fa-dot_circle_o
-  package_uninstalled = " ", -- nf-fa-circle_o
+  package_installed   = "󰧟 ", -- nf-md-circle_medium
+  package_pending     = "󰻃 ", -- nf-md-record_circle_outline
+  package_uninstalled = "󰝦 ", -- nf-md-checkbox_blank_circle_outline
 }
 
 return icons
