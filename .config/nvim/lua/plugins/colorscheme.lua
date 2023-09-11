bc8724df99626be01d1dadd0e7205533ed76aad7
Loong <wangl.cc@outlook.com>
2023-09-11 20:40:06 +0800

    refactor(vim): create base_on function for colorscheme
    The highlight is not cleared when reloading the colorscheme,
    so the `nvim_get_hl` will return the highlight from the previous.
    So we use `base_on` function to create a new highlight group
    with manually specified base highlight group.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,33 +1,5 @@
 local tbl = require "util.table"
 
-local function hl_default(name)
-  local default = require("tokyonight.treesitter").defaults[name]
-    or vim.api.nvim_get_hl(0, { name = name })
-  if vim.tbl_isempty(default) then
-    require("util.log").error("Failed to get highlight group: " .. name, "Highlight")
-  end
-  return default
-end
-
-local function hl_override(hl, name, opts)
-  local g = hl[name]
-  if g and not g.link then
-    for k, v in pairs(opts) do
-      g[k] = v
-    end
-  else -- if g is not defined or g link to other group
-    if not g then g = hl_default(name) end
-    while g.link do
-      g = hl[g.link] or hl_default(g.link)
-    end
-    local g2 = vim.deepcopy(g)
-    for k, v in pairs(opts) do
-      g2[k] = v
-    end
-    hl[name] = g2
-  end
-end
-
 return {
   ---@type LazyPluginSpec
   {
@@ -48,17 +20,29 @@ return {
         floats = "transparent",
       },
       sidebars = { "qf" },
-      on_highlights = function(hl, c)
-        hl.WindowPickerStatusLine = { fg = c.black, bg = c.blue }
-        hl.WindowPickerStatusLineNC = { fg = c.black, bg = c.blue }
-        hl.WindowPickerWinBar = { fg = c.black, bg = c.blue }
-        hl.WindowPickerWinBarNC = { fg = c.black, bg = c.blue }
-        hl_override(hl, "@keyword.function", { style = "italic" }) -- e.g. `function`, `end` in Julia
-        hl_override(hl, "@keyword.operator", { style = "italic" }) -- e.g. `in`, `isa` in Julia, `and`, `or` in Lua
-        hl_override(hl, "Conditional", { style = "italic" }) -- e.g. `if`, `else`, `elseif`, `end` in Julia
-        hl_override(hl, "Repeat", { style = "italic" }) -- e.g. `for`, `while`, `end` in Julia
-        hl_override(hl, "Include", { style = "italic" }) -- e.g. `using`, `import` in Julia
-        hl_override(hl, "Exception", { style = "italic" }) -- e.g. `try`, `catch`, `finally` in Julia
+      on_highlights = function(hls, c)
+        local function base_on(base, opts)
+          local hl = hls[base]
+          if not hl then
+            require("util.log").error(
+              "Highlight group not found: " .. base,
+              "Highlight"
+            )
+          end
+          -- vim.tbl_extend will create new table, so it's safe to modify it.
+          return vim.tbl_extend("force", hl, opts)
+        end
+
+        hls.WindowPickerStatusLine = { fg = c.black, bg = c.blue }
+        hls.WindowPickerStatusLineNC = { fg = c.black, bg = c.blue }
+        hls.WindowPickerWinBar = { fg = c.black, bg = c.blue }
+        hls.WindowPickerWinBarNC = { fg = c.black, bg = c.blue }
+        hls["@keyword.function"] = base_on("Function", { style = "italic" }) -- e.g. `function`, `end` in Julia
+        hls["@keyword.operator"] = base_on("@operator", { style = "italic" }) -- e.g. `in`, `isa` in Julia, `and`, `or` in Lua
+        hls["Conditional"] = base_on("Statement", { style = "italic" }) -- e.g. `if`, `else`, `elseif`, `end` in Julia
+        hls["Repeat"] = base_on("Statement", { style = "italic" }) -- e.g. `for`, `while`, `end` in Julia
+        hls["Include"] = base_on("Statement", { style = "italic" }) -- e.g. `using`, `import` in Julia
+        hls["Exception"] = base_on("Statement", { style = "italic" }) -- e.g. `try`, `catch`, `finally` in Julia
       end,
     },
     config = function(_, opts)
