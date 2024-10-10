173439152da28fc974c72f464eda86427d403131
Loong <40141251+wangl-cc@users.noreply.github.com>
2024-10-10 11:45:36 +0100

    feat(nvim): use rustfmt nightly in lsp

--- .config/nvim/lua/plugins/lang/rust.lua
+++ .config/nvim/lua/plugins/lang/rust.lua
@@ -18,6 +18,9 @@ return {
                     "inactive-code",
                   },
                 },
+                rustfmt = {
+                  extraArgs = { "+nightly" },
+                },
               },
             },
           },
