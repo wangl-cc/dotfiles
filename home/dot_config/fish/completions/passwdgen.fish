fb9f10dbe3e4fa7a946a13ad8863daaff191503f
Loong <git@loongw.cc>
2025-08-24 17:51:13 +0800

    fish: remove removed command passwdgen

--- home/dot_config/fish/completions/passwdgen.fish
+++
@@ -1,8 +0,0 @@
-complete -f -c passwdgen
-complete -c passwdgen -s h -l help -d "Print help message"
-complete -c passwdgen -s e -l example -d "Print examples"
-
-complete -c passwdgen -s p -l pattern -x -d "Pattern used to generate password"
-complete -c passwdgen -s l -l length -x -d "Length of password"
-complete -c passwdgen -s c -l charset -x -a "u l W d w s a" -d \
-  "Charset used to generate password"
